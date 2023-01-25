40854dc7cf2e9941a4c7dc4d3caf8bae1655dc7d
Kratika Kaushik <kratikakaushik27@gmail.com>
2023-01-25 11:19:34 +0530

    added question 4, 5,6

--- index.js
+++ index.js
@@ -11,19 +11,36 @@
 // console.log(reversedString);
 
 //2. How to check type of data
-let r=3;
-console.log(typeof(r));
-r="hdbeiwh";
-console.log(typeof(r));
-r=3.0909;
-console.log(typeof(r));
-r=true;
-console.log(typeof(r));
+// let r=3;
+// console.log(typeof(r));
+// r="hdbeiwh";
+// console.log(typeof(r));
+// r=3.0909;
+// console.log(typeof(r));
+// r=true;
+// console.log(typeof(r));
 
 //3. Empty an array
-let arr=[3,4,5,6,7];
-arr1=arr;
-arr=[];
-console.log(arr);
-console.log(arr1);
+// let arr=[3,4,5,6,7];
+// arr1=arr;
+// arr=[];
+// console.log(arr);
+// console.log(arr1);
+
+//4. Sum of elements in an array
+const arr=[3,1,2,6,7,2];
+let sum=arr.reduce((acc,curr)=>{
+  acc=acc+curr;
+  return acc;
+},0);
+console.log(sum);
+
+//5.How to copy an array into other array
+let a=[1,2,3,4,5,6];
+aa=a;
+console.log(aa);
+console.log(a);
+
+//6. Can we create an array with different datatype? 
+//Ans. Yes
 
