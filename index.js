52176bce537f5f838f22978e41fbac3328332b19
Kratika Kaushik <kratikakaushik27@gmail.com>
2023-01-25 11:05:34 +0530

    added solution of ques 2,3

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
 
 //1.Reverse the letters of the words
-let reversedString=reverse("Hello There!");
-function reverse(s){
-  let stringg=s.split(" ");
-  let reverseString=stringg.map(a=>{
-    return a.split("").reverse().join("");
-  });
-  return reverseString.join(" ");
-}
-console.log(reversedString);
+// let reversedString=reverse("Hello There!");
+// function reverse(s){
+//   let stringg=s.split(" ");
+//   let reverseString=stringg.map(a=>{
+//     return a.split("").reverse().join("");
+//   });
+//   return reverseString.join(" ");
+// }
+// console.log(reversedString);
+
+//2. How to check type of data
+let r=3;
+console.log(typeof(r));
+r="hdbeiwh";
+console.log(typeof(r));
+r=3.0909;
+console.log(typeof(r));
+r=true;
+console.log(typeof(r));
+
+//3. Empty an array
+let arr=[3,4,5,6,7];
+arr1=arr;
+arr=[];
+console.log(arr);
+console.log(arr1);
+
