2f89adf0b2394566d556b7455df5c2166ceafc79
Kratika Kaushik <kratikakaushik27@gmail.com>
2023-01-28 00:50:32 +0530

    Question 7 and 8

--- index.js
+++ index.js
@@ -28,19 +28,35 @@
 // console.log(arr1);
 
 //4. Sum of elements in an array
-const arr=[3,1,2,6,7,2];
-let sum=arr.reduce((acc,curr)=>{
-  acc=acc+curr;
-  return acc;
-},0);
-console.log(sum);
+// const arr=[3,1,2,6,7,2];
+// let sum=arr.reduce((acc,curr)=>{
+//   acc=acc+curr;
+//   return acc;
+// },0);
+// console.log(sum);
 
 //5.How to copy an array into other array
-let a=[1,2,3,4,5,6];
-aa=a;
-console.log(aa);
-console.log(a);
+// let a=[1,2,3,4,5,6];
+// aa=a;
+// console.log(aa);
+// console.log(a);
 
 //6. Can we create an array with different datatype? 
 //Ans. Yes
 
+//7. Merge two arrays with different datatypes with duplicates
+let x=[1,2,"Hello",8,"Hi"];
+y=[6,3,"Hi",1,4];
+console.log(x.concat(y));
+
+//8. Merge two arrays with differnt datatypes without duplicates
+
+let arr1=[1,2,5,9,11,45]
+let arr2=[4,8,2,11,49]
+let arr3=arr1.concat(arr2);
+let unique=[];
+arr3.forEach((i)=>{
+    if(unique.indexOf(i)===-1)
+    unique.push(i);
+});
+console.log(unique)
